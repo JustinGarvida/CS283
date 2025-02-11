5236c63e2064c20990844ee8ec3f6b4a4b8e83bb
JustinGarvida <justingarvida1@gmail.com>
2025-02-10 19:29:31 -0500

    fixed small issues

--- 3-Drexel_Shell_Part_1/starter/dsh_cli.c
+++ 3-Drexel_Shell_Part_1/starter/dsh_cli.c
@@ -36,59 +36,41 @@ int main()
         fprintf(stderr, "Error: Failed to allocate memory\n");
         return EXIT_FAILURE;
     }
-
     command_list_t clist;
-
     while (1)
     {
-        // Prompt user for input
         printf("%s", SH_PROMPT);
-        if (fgets(cmd_buff, SH_CMD_MAX, stdin) == NULL)
-        {
+        if (fgets(cmd_buff, SH_CMD_MAX, stdin) == NULL) {
             printf("\n");
             break;
         }
-
-        // Remove the trailing newline character
         cmd_buff[strcspn(cmd_buff, "\n")] = '\0';
-
-        // Exit command check
-        if (strcmp(cmd_buff, EXIT_CMD) == 0)
-        {
+        if (strcmp(cmd_buff, EXIT_CMD) == 0) {
             free(cmd_buff);
             return EXIT_SUCCESS;
         }
 
-        // Dragon command check
-        if (strcmp(cmd_buff, "dragon") == 0)
-        {
+        if (strcmp(cmd_buff, "dragon") == 0) {
             printf("%s", DRAGON_IMAGE);
             continue;
         }
-
-        //Building Command List
         int rc = build_cmd_list(cmd_buff, &clist);
         if (rc == OK)
         {
             printf(CMD_OK_HEADER, clist.num);
-            for (int i = 0; i < clist.num; i++)
-            {
-                if (strlen(clist.commands[i].args) > 0)
-                {
+            for (int i = 0; i < clist.num; i++) {
+                if (strlen(clist.commands[i].args) > 0) {
                     printf("<%d> %s [%s]\n", i + 1, clist.commands[i].exe, clist.commands[i].args);
                 }
-                else
-                {
+                else {
                     printf("<%d> %s\n", i + 1, clist.commands[i].exe);
                 }
             }
         }
-        else if (rc == ERR_TOO_MANY_COMMANDS)
-        {
+        else if (rc == ERR_TOO_MANY_COMMANDS) {
             printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
         }
-        else if (rc == WARN_NO_CMDS)
-        {
+        else if (rc == WARN_NO_CMDS) {
             printf(CMD_WARN_NO_CMD);
         }
     }
