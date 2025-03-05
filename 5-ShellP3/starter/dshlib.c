3f215efcb854a6383312a3827165fe63081984eb
JustinGarvida <justingarvida1@gmail.com>
2025-03-04 20:08:49 -0500

    revert

--- 5-ShellP3/starter/dshlib.c
+++ 5-ShellP3/starter/dshlib.c
@@ -8,6 +8,50 @@
 #include <sys/wait.h>
 #include "dshlib.h"
 
+/*
+ * Implement your exec_local_cmd_loop function by building a loop that prompts the
+ * user for input.  Use the SH_PROMPT constant from dshlib.h and then
+ * use fgets to accept user input.
+ *
+ *      while(1){
+ *        printf("%s", SH_PROMPT);
+ *        if (fgets(cmd_buff, ARG_MAX, stdin) == NULL){
+ *           printf("\n");
+ *           break;
+ *        }
+ *        //remove the trailing \n from cmd_buff
+ *        cmd_buff[strcspn(cmd_buff,"\n")] = '\0';
+ *
+ *        //IMPLEMENT THE REST OF THE REQUIREMENTS
+ *      }
+ *
+ *   Also, use the constants in the dshlib.h in this code.
+ *      SH_CMD_MAX              maximum buffer size for user input
+ *      EXIT_CMD                constant that terminates the dsh program
+ *      SH_PROMPT               the shell prompt
+ *      OK                      the command was parsed properly
+ *      WARN_NO_CMDS            the user command was empty
+ *      ERR_TOO_MANY_COMMANDS   too many pipes used
+ *      ERR_MEMORY              dynamic memory management failure
+ *
+ *   errors returned
+ *      OK                     No error
+ *      ERR_MEMORY             Dynamic memory management failure
+ *      WARN_NO_CMDS           No commands parsed
+ *      ERR_TOO_MANY_COMMANDS  too many pipes used
+ *
+ *   console messages
+ *      CMD_WARN_NO_CMD        print on WARN_NO_CMDS
+ *      CMD_ERR_PIPE_LIMIT     print on ERR_TOO_MANY_COMMANDS
+ *      CMD_ERR_EXECUTE        print on execution failure of external command
+ *
+ *  Standard Library Functions You Might Want To Consider Using (assignment 1+)
+ *      malloc(), free(), strlen(), fgets(), strcspn(), printf()
+ *
+ *  Standard Library Functions You Might Want To Consider Using (assignment 2+)
+ *      fork(), execvp(), exit(), chdir()
+ */
+
 int exec_local_cmd_loop()
 {
     cmd_buff_t cmd_buff;
@@ -27,40 +71,19 @@ int exec_local_cmd_loop()
             break;
         }
         cmd_buff._cmd_buffer[strcspn(cmd_buff._cmd_buffer, "\n")] = '\0';
-
-        // Handle built-in commands directly
-        if (strcmp(cmd_buff._cmd_buffer, EXIT_CMD) == 0)
-        {
-            free_cmd_buff(&cmd_buff);
-            return OK_EXIT;
-        }
-        if (strcmp(cmd_buff._cmd_buffer, "dragon") == 0)
-        {
-            printf("%s", DRAGON_IMAGE);
-            continue;
-        }
-
-        // Parse the command line into a list of commands
-        command_list_t cmd_list;
-        rc = build_cmd_list(cmd_buff._cmd_buffer, &cmd_list);
+        rc = build_cmd_buff(cmd_buff._cmd_buffer, &cmd_buff);
         if (rc == WARN_NO_CMDS)
         {
             fprintf(stderr, CMD_WARN_NO_CMD);
             continue;
         }
-        else if (rc == ERR_TOO_MANY_COMMANDS)
+        else if (rc == ERR_MEMORY)
         {
-            fprintf(stderr, CMD_ERR_PIPE_LIMIT, CMD_MAX);
-            continue;
+            fprintf(stderr, "Error: Command buffer memory allocation failed\n");
+            break;
         }
-        else if (rc != OK)
-        {
-            fprintf(stderr, "Error: Command parsing failed\n");
-            continue;
-        }
+        Built_In_Cmds cmd_type = match_command(cmd_buff.argv[0]);
 
-        // Handle built-in commands
-        Built_In_Cmds cmd_type = match_command(cmd_list.commands[0].argv[0]);
         if (cmd_type != BI_NOT_BI)
         {
             Built_In_Cmds exec_result = exec_built_in_cmd(&cmd_buff);
@@ -71,93 +94,13 @@ int exec_local_cmd_loop()
             }
             continue;
         }
-
-        // Execute piped commands
-        rc = exec_piped_commands(&cmd_list);
+        rc = exec_cmd(&cmd_buff);
         if (rc != OK)
         {
-            // fprintf(stderr, CMD_ERR_EXECUTE);
+            return ERR_EXEC_CMD;
         }
     }
     free_cmd_buff(&cmd_buff);
-    return OK;
-}
-
-int exec_piped_commands(command_list_t *cmd_list)
-{
-    int num_commands = cmd_list->num;
-    int pipefds[2 * (num_commands - 1)];
-    int status;
-    pid_t pid;
-
-    // Create pipes
-    for (int i = 0; i < num_commands - 1; i++)
-    {
-        if (pipe(pipefds + i * 2) < 0)
-        {
-            perror("pipe");
-            return ERR_EXEC_CMD;
-        }
-    }
-
-    // Execute commands
-    for (int i = 0; i < num_commands; i++)
-    {
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            return ERR_EXEC_CMD;
-        }
-        else if (pid == 0)
-        {
-            // Child process
-            if (i != 0)
-            {
-                // Redirect stdin from the previous pipe
-                if (dup2(pipefds[(i - 1) * 2], STDIN_FILENO) < 0)
-                {
-                    perror("dup2");
-                    exit(EXIT_FAILURE);
-                }
-            }
-            if (i != num_commands - 1)
-            {
-                // Redirect stdout to the next pipe
-                if (dup2(pipefds[i * 2 + 1], STDOUT_FILENO) < 0)
-                {
-                    perror("dup2");
-                    exit(EXIT_FAILURE);
-                }
-            }
-
-            // Close all pipe file descriptors
-            for (int j = 0; j < 2 * (num_commands - 1); j++)
-            {
-                close(pipefds[j]);
-            }
-
-            // Execute the command using argv
-            if (execvp(cmd_list->commands[i].argv[0], cmd_list->commands[i].argv) == -1)
-            {
-                perror("execvp");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
-
-    // Close all pipe file descriptors in the parent
-    for (int i = 0; i < 2 * (num_commands - 1); i++)
-    {
-        close(pipefds[i]);
-    }
-
-    // Wait for all child processes to finish
-    for (int i = 0; i < num_commands; i++)
-    {
-        wait(&status);
-    }
-
     return OK;
 }
 
@@ -316,7 +259,7 @@ int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff)
             current_pointer++;
         current_pointer++;
     }
-    cmd_buff->argv[cmd_buff->argc] = NULL; // Ensure argv is null-terminated
+    cmd_buff->argv[cmd_buff->argc] = NULL;
     if (cmd_buff->argc > 0)
     {
         return OK;
@@ -325,68 +268,4 @@ int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff)
     {
         return WARN_NO_CMDS;
     }
-}
-
-int build_cmd_list(char *cmd_line, command_list_t *clist)
-{
-    if (cmd_line == NULL || clist == NULL)
-    {
-        return WARN_NO_CMDS;
-    }
-
-    // Initialize the command list
-    memset(clist, 0, sizeof(command_list_t));
-
-    // Tokenize the command line by pipes
-    char *command = strtok(cmd_line, PIPE_STRING);
-    int command_count = 0;
-
-    if (command == NULL)
-    {
-        return WARN_NO_CMDS;
-    }
-
-    while (command != NULL)
-    {
-        if (command_count >= CMD_MAX)
-        {
-            return ERR_TOO_MANY_COMMANDS;
-        }
-
-        // Skip leading spaces in the command
-        while (*command == SPACE_CHAR)
-        {
-            command++;
-        }
-
-        // Check if the command is too long
-        if (strlen(command) >= SH_CMD_MAX)
-        {
-            return ERR_CMD_OR_ARGS_TOO_BIG;
-        }
-
-        // Allocate memory for the command buffer
-        int rc = alloc_cmd_buff(&clist->commands[command_count]);
-        if (rc != OK)
-        {
-            return ERR_MEMORY;
-        }
-
-        // Copy the command into the buffer
-        strncpy(clist->commands[command_count]._cmd_buffer, command, SH_CMD_MAX - 1);
-        clist->commands[command_count]._cmd_buffer[SH_CMD_MAX - 1] = '\0';
-
-        // Parse the command into arguments
-        rc = build_cmd_buff(clist->commands[command_count]._cmd_buffer, &clist->commands[command_count]);
-        if (rc != OK)
-        {
-            return rc;
-        }
-
-        command_count++;
-        command = strtok(NULL, PIPE_STRING);
-    }
-
-    clist->num = command_count;
-    return OK;
 }
